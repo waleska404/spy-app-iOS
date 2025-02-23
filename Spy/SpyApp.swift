d301e081778bf6b69b10f28c09515548c17c790b
Paula Waleska <paulaboyanoivars@gmail.com>
2025-02-23 12:04:48 +0100

    Initial Commit

---
+++ Spy/SpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpyApp.swift
+//  Spy
+//
+//  Created by Paula Waleska on 23/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SpyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpyTests/SpyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpyTests.swift
+//  SpyTests
+//
+//  Created by Paula Waleska on 23/2/25.
+//
+
+import Testing
+@testable import Spy
+
+struct SpyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
